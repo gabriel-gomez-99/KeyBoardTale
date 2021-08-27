7ddc00a123428ea831f1192e36c88c0869091fc2
Gabriel Gomez <tete1657@gmail.com>
2021-08-27 19:22:22 -0300

    Commit Inicial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'KeyBoardTale'`, () => {
+  it(`should have as title 'Guzman'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('KeyBoardTale');
+    expect(app.title).toEqual('Guzman');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('KeyBoardTale app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Guzman app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+
+import { SharedModule } from './shared/shared.module';
+import { MaterialModule } from './material/material.module';
+import { ErrorPagesModule } from './error-pages/error-pages.module';
+import { HomeModule } from './home/home.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    MaterialModule,
+    ErrorPagesModule,
+    HomeModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
